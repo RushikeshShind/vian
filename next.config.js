ab5008562ace422d98fb121feb15f45b8f9e7f23
RushikeshShind <Rushikeshshinde57323@gmail.com>
2024-11-19 15:11:53 +0530

    confi update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-module.exports = withBundleAnalyzer({});
+module.exports = {
+  reactStrictMode: true,
+  // Add other configurations as needed
+};
